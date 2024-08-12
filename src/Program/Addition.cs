705d9d74cf5d613d547be180b756faa5f50df073
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-08-12 10:59:14 -0300

    agregué la division

---
+++ src/Program/Addition.cs
@@ -0,0 +1,7 @@
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,29 +1,13 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
+Console.WriteLine(Division.Divide(18, 2)); 
 
-//esta clase implementa la operación suma
-public class Addition
+//operación división
+public class Division
 {
-    public static int Add(int a, int b)
+    public static double Divide(int a, int b)
     {
-        return a + b;
-    }
-}
-//Encargado de restar
-public class Substraction
-{
-    public static int Substract(int a, int b)
-    {
-        return a - b;
-    }
-}
-//Operación multiplicación
-public class Multiplication
-{
-    public static int Multiply(int a, int b)
-    {
-        return a * b;
+        return (double)a / b;
     }
 }
