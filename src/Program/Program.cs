549ff30dd0fb8e4fa581e0425d8f88b4ad6442e0
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2024-08-12 10:19:14 -0300

    Se engarga de restar bien

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,11 @@
-﻿//Encargado de restar
+﻿/// //Hola a todos
+Console.WriteLine(Addition.Add(7, 2));
+Console.WriteLine(Substraction.Substract(10, 1));
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+//FPP
+
+//Encargado de restar
 public class Substraction
 {
     public static int Substract(int a, int b)
