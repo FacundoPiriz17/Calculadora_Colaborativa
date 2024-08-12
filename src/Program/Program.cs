53006ce4fa053bd6dbe166722ba03db4bd5b0531
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-08-12 11:31:07 -0300

    fix main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,13 +1,21 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2)); 
+Console.WriteLine(Division.Divide(18, 2));
 
-//operación división
-public class Division
+//esta clase implementa la operación suma
+public class Addition
 {
-    public static double Divide(int a, int b)
+    public static int Add(int a, int b)
     {
-        return (double)a / b;
+        return a + b;
+    }
+}
+//Encargado de restar
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
     }
 }
